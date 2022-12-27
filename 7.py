c38744cc6d291ec79375a9cdf2ae060431727f37
janaro <Power2004@yandex.ru>
2022-12-28 01:31:39 +0300

    update

---
+++ 7.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+# https://t.me/test_for_joparo_bot
+token = "5227484175:AAEWGEcfa1q0gMA4HfAEL843Qk7NFdOe1Cw"
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информациюо МТУСИ?', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею...')
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда тебе сюда - https://mtuci.ru/')
+
+bot.polling(none_stop=True)
